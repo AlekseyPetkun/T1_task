72059eef343e873bc6bbf23d4dd780cfa57ef421
Aleksey Petkun <aleksey_2710@mail.ru>
2023-09-26 22:13:36 +0300

    creating openapi.yaml

--- src/main/java/com/github/alekseypetkun/t1_task/T1TaskApplication.java
+++ src/main/java/com/github/alekseypetkun/t1_task/T1TaskApplication.java
@@ -1,9 +1,15 @@
 package com.github.alekseypetkun.t1_task;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "T1TaskApplication",
+        description = "Тестовое задание от Т1 Консалтинг"
+))
 public class T1TaskApplication {
 
     public static void main(String[] args) {
