4f26f08a68e133f86b739ba61cbe9591aa2f7b3a
Aleksey Petkun <aleksey_2710@mail.ru>
2023-09-26 19:36:06 +0300

    initial commit

---
+++ src/main/java/com/github/alekseypetkun/t1_task/T1TaskApplication.java
@@ -0,0 +1,13 @@
+package com.github.alekseypetkun.t1_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1TaskApplication.class, args);
+    }
+
+}
